903e2b79141633353685e3b31c92430f7d929510
Karim-off <153532713+Karim-off@users.noreply.github.com>
2023-12-14 12:37:17 -0400

    Actualizar plugins

---
+++ plugins/gc-masc14.js
@@ -0,0 +1,41 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `𝘓𝘐𝘚𝘛𝘈 𝘔𝘈𝘚𝘊❤️‍🔥•: ${pesan}`
+let teks = `╭┈┈ ๑👻๑ •• ${oi} ๑👻๑ ••:\n`
+for (let mem of participants) {
+teks += `@${mem.id.split('@')[0]}❤️‍🔥`}
+teks += `ㅤ
+│
+│         🐻‍❄️ 𝐀𝐝𝐫𝐢𝐞𝐥𝐁𝐨𝐭 🐻‍❄️
+│        ₊˚︶︶︶︶︶︶︶︶˚ ‧₊
+│
+│      ෆ 𝘌𝘯𝘤𝘢𝘳𝘨𝘢𝘥o:
+│      ෆ 𝘏𝘰𝘳𝘢𝘳𝘪𐐫:
+│         ‧₊⏱️ 14 🇵🇪 ⌇ 15 🇧🇴 ⌇ 16 🇦🇷      
+│
+│ㅤㅤʚ 𝙹𝚄𝙶𝙰𝙳𝙾𝚁𝙴𝚂❄️:
+│ㅤ🐼• 
+│ㅤ🐼• 
+│ㅤ🐼• 
+│ㅤ🐼•
+│   ㅤㅤ
+│ㅤㅤʚ 𝚂𝚄𝙿𝙻𝙴𝙽𝚃𝙴𝚂❄️:
+│ㅤ🐼•
+│ㅤ🐼•
+│
+│   ⊹ ִֶָ𝙳𝙾𝙽𝙰𝙳𝙾𝚁 𝙳𝙴 𝚂𝙰𝙻𝙰: 
+│ㅤ
+│. 🐼•
+╰───────────────๑ •`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['smsm14 <mesaje>','masc14 <mesaje>']
+handler.tags = ['group']
+handler.command = /^(smsm14|masc14)$/i
+handler.admin = true
+handler.group = true
+export default handler
