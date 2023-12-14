6cf6f91e5984dd53700680b50a8d6abacf234e37
Karim-off <153532713+Karim-off@users.noreply.github.com>
2023-12-14 12:55:03 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
-let oi = `*@ventas_bot_ludmi* ${pesan}`
-let teks = `*ACTIVENSE PLANTAS*\n\n ${oi}\n\n🫶🏻 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
+let oi = `*@usxr._.karim* ${pesan}`
+let teks = `𝙍𝙀𝙑𝙄𝙑𝘼𝙉 𝘽𝙊𝙏𝙎\n\n ${oi}\n\n🫶🏻 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
 for (let mem of participants) {
-teks += `💋➤  @${mem.id.split('@')[0]}\n`}
-teks += `*🐾LudmiBot*\n\n*<3*`
+teks += `💋│☭  @${mem.id.split('@')[0]}\n`}
+teks += `*🐼𝐀𝐃𝐑𝐈𝐄𝐋-𝐁𝐎𝐓*\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
