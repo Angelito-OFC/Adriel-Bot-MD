ef8d5b875f1d2669d2db6063473774d478babf72
Karim-off <153532713+Karim-off@users.noreply.github.com>
2023-12-17 21:59:01 -0400

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ADRIEL BOT🗣️', body: '©ventas_bot.karim', sourceUrl: `https://github.com/Karim-off/Adriel-Bot-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: 'ADRIEL BOT🗣️', body: '©usxr._.karim', sourceUrl: `https://github.com/Karim-off/Adriel-Bot-MD`, thumbnail: imagen2}}})}
     
 return !0 }
 handler.register = true
