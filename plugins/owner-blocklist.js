a1fe787ed86c41d3e9f435e018074b1462c76bab
Karim-off <153532713+Karim-off@users.noreply.github.com>
2023-12-17 22:49:26 -0400

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 await conn.fetchBlocklist().then(async data => {
 let txt = `*≡ Lista de bloqueados*\n*LIST OF BANS*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
 for (let i of data) {
-txt += `▢ @${i.split("@")[0]}\n`}
+txt += `│☭ @${i.split("@")[0]}\n`}
 txt += "└───────────"
 return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 }).catch(err => {
